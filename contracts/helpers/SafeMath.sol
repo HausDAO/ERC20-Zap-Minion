376063e7f5defec846c16e6f2613dca01f484767
William Warren <williamwarren@williams-mbp-2.home>
2021-05-24 21:40:35 -0400

    initial commit

---
+++ contracts/helpers/SafeMath.sol
@@ -0,0 +1,16 @@
+library SafeMath { // arithmetic wrapper for unit under/overflow check
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b);
+        return c;
+    }
+    
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        return c;
+    }
+}
